66cdd1a6e5f55ca3861cbbf0ff0c797baa652411
Andy Macdonald <hello@andymacdonald.dev>
2022-02-07 15:22:32 +0000

    Basic project for mentoring purposes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis } from 'recharts'
+import './App.css'
 
-function App() {
+const mockData = [
+  { name: 'Page A', uv: 1000, pv: 2400, amt: 2400, uvError: [75, 20] },
+  { name: 'Page B', uv: 300, pv: 4567, amt: 2400, uvError: [90, 40] },
+  { name: 'Page C', uv: 280, pv: 1398, amt: 2400, uvError: 40 },
+  { name: 'Page D', uv: 200, pv: 9800, amt: 2400, uvError: 20 },
+  { name: 'Page E', uv: 278, pv: null, amt: 2400, uvError: 28 },
+  { name: 'Page F', uv: 189, pv: 4800, amt: 2400, uvError: [90, 20] },
+  { name: 'Page G', uv: 189, pv: 4800, amt: 2400, uvError: [28, 40] },
+  { name: 'Page H', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+  { name: 'Page I', uv: 189, pv: 4800, amt: 2400, uvError: 28 },
+  { name: 'Page J', uv: 189, pv: 4800, amt: 2400, uvError: [15, 60] }
+]
+
+function App () {
+  const [data, setData] = React.useState([])
+
+  React.useEffect(() => {
+    setData(mockData)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div>
+        <LineChart
+          width={400}
+          height={400}
+          data={data}
+          margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
         >
-          Learn React
-        </a>
-      </header>
+          <XAxis dataKey='name' />
+          <Tooltip />
+          <CartesianGrid stroke='#f5f5f5' />
+          <Line type='monotone' dataKey='uv' stroke='#ff7300' yAxisId={0} />
+          <Line type='monotone' dataKey='pv' stroke='#387908' yAxisId={1} />
+        </LineChart>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
